ae11a2851106e150ec46c65d72ad344297dde82b
Josh Levy <joshaaronlevy@gmail.com>
2021-01-13 05:27:25 -0700

    "Added filter to skills"

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -10,12 +10,15 @@ export class PresentationComponent implements OnInit, OnDestroy {
   date: Date = new Date();
   skills: any;
   skill: any;
+  filter: any;
+  filteredSkills: any;
 
   constructor(
     public skillService: SkillService
   ) {}
 
   ngOnInit() {
+    this.filter = 'All';
     const body = document.getElementsByTagName('body')[0];
     body.classList.add('presentation-page');
     const navbar = document.getElementsByTagName('nav')[0];
@@ -26,8 +29,24 @@ export class PresentationComponent implements OnInit, OnDestroy {
   getSkills() {
     return this.skillService.getSkills().subscribe(data => {
       this.skills = data.skills;
-      console.log(this.skills);
+      this.filterSkills(this.filter);
     });
+  }
+
+  filterSkills(filter) {
+    this.filter = filter;
+    this.filteredSkills = [];
+    for (let i = 0; i < this.skills.length; i++) {
+      if (this.filter !== 'All') {
+        if (this.skills[i].type === this.filter) {
+          this.filteredSkills.push(this.skills[i]);
+        }
+      }
+      else {
+        this.filteredSkills.push(this.skills[i]);
+      }
+    }
+    console.log(this.filteredSkills);
   }
 
   ngOnDestroy() {
