27acd6622337a0739a4bd28194a791c208970517
Josh Levy <joshaaronlevy@gmail.com>
2021-02-12 23:24:39 -0700

    "Updated conditional job details function"

--- src/app/components/work-history/work-history.component.ts
+++ src/app/components/work-history/work-history.component.ts
@@ -9,8 +9,9 @@ import { JobService } from 'app/services/job.service';
 })
 export class WorkHistoryComponent implements OnInit {
   jobs: Jobs[] = [];
-  jobDetails: any;
-  expandedJobs: number[] = [];
+  jobDetails: number;
+  // expandedJobs: number[] = [];
+  expandedJobs: {[jobId: number]: boolean} = {};
 
   constructor(
     public jobService: JobService
@@ -26,7 +27,6 @@ export class WorkHistoryComponent implements OnInit {
       this.jobs.sort(function (a, b) {
         return a.id - b.id;
       });
-      // console.log(this.jobs);
     });
   }
 
@@ -46,18 +46,28 @@ export class WorkHistoryComponent implements OnInit {
   //   console.log(this.expandedJobs);
   // }
 
+  // Checking which job details are being displayed from the expandedJobs array
+  // displayDetails(selectedJob) {
+  //   this.jobDetails = selectedJob.id;
+  //   if (!this.isExpanded(this.jobDetails)) {
+  //     this.expandedJobs.push(selectedJob.id);
+  //   } else {
+  //     this.expandedJobs.splice(this.expandedJobs.indexOf(selectedJob.id), 1);
+  //   }
+  //   console.log(this.expandedJobs);
+  // }
+
+  // Object key based tracking
   displayDetails(selectedJob) {
     this.jobDetails = selectedJob.id;
     if (!this.isExpanded(this.jobDetails)) {
-      this.expandedJobs.push(selectedJob.id);
+      this.expandedJobs[this.jobDetails] = true;
     } else {
-      this.expandedJobs.splice(this.expandedJobs.indexOf(selectedJob.id), 1);
+      this.expandedJobs[this.jobDetails] = false;
     }
-    console.log(this.expandedJobs);
   }
 
   isExpanded(jobId: number) {
-    return this.expandedJobs.includes(jobId);
+    return this.expandedJobs[jobId];
   }
-
 }
