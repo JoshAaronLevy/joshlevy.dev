2ceb6421c7e4d0d41f7844073d916d62ad3b0035
Josh Levy <joshaaronlevy@gmail.com>
2022-07-10 17:49:04 -0600

    Updated work history

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,21 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
-  constructor(
-    public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig
-  ) {}
+	constructor(
+		public ref: DynamicDialogRef,
+		public config: DynamicDialogConfig
+	) { }
 
-  ngOnInit(): void {}
+	ngOnInit(): void { }
 
-  closeModal() {
-    this.ref.close();
-  }
+	closeModal() {
+		this.ref.close();
+	}
 
 }
